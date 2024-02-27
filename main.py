93ea07bfbc2328a3bb169c6116fafaa604083c19
Erick Silva <engs.3003@gmail.com>
2024-02-27 16:42:03 -0300

    editing comments

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+# Imports para o projeto
 from selenium import webdriver
 import time
 
-
+#Nomeando o driver do Chrome para utilização no código
 nav = webdriver.Chrome()
 
+#Navegando até o site do Mercado livre
 nav.get("https://mercadolivre.com.br")
 
 # Esperando alguns segundos para a página carregar completamente
 time.sleep(2)
 
-# Exibindo o título da página para verificar se a navegação foi bem-sucedida
-print("Título da página:", nav.title)
-
+#Saindo do navegador
 nav.quit()
