66bddd77f682ec944ff9ea43c936383dd7601c08
Erick Silva <engs.3003@gmail.com>
2024-02-27 19:56:50 -0300

    adding sheet reading snippet

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 # Imports para o projeto
 from selenium import webdriver
 import time
+import openpyxl
+
+def ler_planilha(nome_arq):
+    wb = openpyxl.load_workbook(nome_arq)
+
+    planilha = wb.active
+
+    items = []
+
+    for linha in planilha.iter_rows(values_only=True):
+        items.append(linha)
+
+    return items
+
+nome_arq = "Lista.xlsx"
+
+
+for linha in ler_planilha(nome_arq):
+    print(linha)
 
 #Nomeando o driver do Chrome para utilização no código
 nav = webdriver.Chrome()
