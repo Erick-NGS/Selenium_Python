89bf5fc700a7bfe6eac453321fa2c4b3edad0b63
Erick Silva <engs.3003@gmail.com>
2024-02-27 23:12:09 -0300

    adding finding name on first item of results query

--- main.py
+++ main.py
@@ -34,10 +34,14 @@ time.sleep(3)
 
 for item in ler_planilha(nome_arq):
     campo_de_busca = nav.find_element(By.TAG_NAME, "INPUT")
-
     campo_de_busca.send_keys(item)
     campo_de_busca.send_keys(Keys.ENTER)
     time.sleep(5)
+
+    nome_produto = nav.find_element(By.XPATH, "//*[@id=*]/div[2]/div/div[2]/a")
+    print(nome_produto.text)
+    time.sleep(5)
+
 
     campo_de_busca = nav.find_element(By.TAG_NAME, "INPUT")
     campo_de_busca.send_keys(Keys.CONTROL + "a")
