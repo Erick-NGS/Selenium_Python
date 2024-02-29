d9b43132dca3351dddcbb3cd6c1816f65e09bf6c
Erick Silva <engs.3003@gmail.com>
2024-02-29 00:37:14 -0300

    new func enviar_email

--- envio_email.py
+++ envio_email.py
@@ -6,26 +6,27 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email_info import sender_email, sender_password, receiver_email, email_subject, email_body, server_name, port_number, filename
 
-email_message = MIMEMultipart()
-email_message["From"] = sender_email
-email_message["To"] = receiver_email
-email_message["Subject"] = email_subject
+def enviar_email():
+    email_message = MIMEMultipart()
+    email_message["From"] = sender_email
+    email_message["To"] = receiver_email
+    email_message["Subject"] = email_subject
 
-email_message.attach(MIMEText(email_body, "plain"))
+    email_message.attach(MIMEText(email_body, "plain"))
 
-email_att = open(filename, "rb")
-part = MIMEBase("application", "octet-stream")
-part.set_payload(email_att.read())
+    email_att = open(filename, "rb")
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(email_att.read())
 
-encoders.encode_base64(part)
+    encoders.encode_base64(part)
 
-part.add_header("Content-Disposition", f"attachment; filename = {filename}",)
+    part.add_header("Content-Disposition", f"attachment; filename = {filename}",)
 
-email_message.attach(part)
-text = email_message.as_string()
+    email_message.attach(part)
+    text = email_message.as_string()
 
-context = ssl.create_default_context()
-server = smtplib.SMTP_SSL(server_name, port_number, context=context)
-server.login(sender_email, sender_password)
-server.sendmail(sender_email, receiver_email, text)
-server.quit()
+    context = ssl.create_default_context()
+    server = smtplib.SMTP_SSL(server_name, port_number, context=context)
+    server.login(sender_email, sender_password)
+    server.sendmail(sender_email, receiver_email, text)
+    server.quit()
